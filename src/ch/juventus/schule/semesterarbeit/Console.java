66977b1b0950546ba78b35282474f73ec86b4c4e
christianpetri <christianpetri@hotmail.com>
2018-04-04 02:57:18 +0200

    Initial commit

---
+++ src/ch/juventus/schule/semesterarbeit/Console.java
@@ -0,0 +1,11 @@
+package ch.juventus.schule.semesterarbeit;
+
+/**
+ * @author : ${user}
+ * @since: ${date}
+ */
+public class Console {
+    public Console(){
+        System.out.print("Hello World");
+    }
+}
